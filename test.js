b57a12a18713dbb2beb676fc939c1969da3936e7
shadab <shadab14meb346@gmail.com>
2023-10-20 20:08:36 +0530

    test 2

--- test.js
+++ test.js
@@ -4,5 +4,6 @@ const NODE_VERSION = '12';
 
 // if we disable the formatter and do some changes which is different of the formatter rules in .prettierrc.js
 const inconsistentFormatting = 'Hey there';
+const anotherInconsistentFormatting = 'Hey there';
 // and then try to commit it ideally husky should fix those formatting issue and then it should go to commit
 // now after the commit we should we 'Hey there' in the file i.e with single quotes
